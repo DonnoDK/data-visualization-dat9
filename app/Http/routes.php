3585be7bd67b809288c8b90587864812d7164738
unknown <dbni11@student.aau.dk>
2015-09-23 14:45:20 +0200

    Benjamin..

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,5 +19,5 @@ Route::get('/Api/Test/create/{name}', function($name){
 	$testPerson = App\testPerson::firstOrCreate(['name' => $name]);
 	$testPerson->test_case()->save($testCase);
 	$testPerson->push();
-	return $testCase->id;
+	return $testCase->id . " appended to user " . $testPerson->name . ' with id ' . $testPerson->id;
 });
